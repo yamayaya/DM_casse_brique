633298ff45bbee5d63552fb47f5710b89bcaae6e
yamayaya <117632350+yamayaya@users.noreply.github.com>
2022-11-17 11:50:50 +0100

    Update cassebrique.py

--- cassebrique.py
+++ cassebrique.py
@@ -1,5 +1,12 @@
 import Pyxel 
 pyxel.init(128, 128, title="Nuit du c0de")
-class jeu:
-  def draw():
+def vaisseau_deplacement(x):
+
+    if pyxel.btn(pyxel.KEY_RIGHT):
+        if (x < 120) :
+            x = x + 1
+    if pyxel.btn(pyxel.KEY_LEFT):
+        if (x > 0) :
+            x = x - 1
+    return x
     
