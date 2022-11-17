a9d2f1409cfe9e7f671593d86b8e74af0eae651a
yamayaya <117632350+yamayaya@users.noreply.github.com>
2022-11-17 11:18:58 +0100

    Update cassebrique.py

--- cassebrique.py
+++ cassebrique.py
@@ -10,11 +10,20 @@ pyxel.init(128, 128, title="Nuit du c0de")
 vaisseau_x = 60
 vaisseau_y = 60
 
+# vies
+vies = 4
+
 # initialisation des tirs
 tirs_liste = []
 
 # initialisation des ennemis
 ennemis_liste = []
+
+# initialisation des explosions
+explosions_liste = []
+
+# chargement des images
+pyxel.load("images.pyxres")
 
 
 def vaisseau_deplacement(x, y):
@@ -73,13 +82,49 @@ def ennemis_deplacement(ennemis_liste):
     return ennemis_liste
 
 
+def vaisseau_suppression(vies):
+    """disparition du vaisseau et d'un ennemi si contact"""
+
+    for ennemi in ennemis_liste:
+        if ennemi[0] <= vaisseau_x+8 and ennemi[1] <= vaisseau_y+8 and ennemi[0]+8 >= vaisseau_x and ennemi[1]+8 >= vaisseau_y:
+            ennemis_liste.remove(ennemi)
+            vies -= 1
+            # on ajoute l'explosion
+            explosions_creation(vaisseau_x, vaisseau_y)
+    return vies
+
+
+def ennemis_suppression():
+    """disparition d'un ennemi et d'un tir si contact"""
+
+    for ennemi in ennemis_liste:
+        for tir in tirs_liste:
+            if ennemi[0] <= tir[0]+1 and ennemi[0]+8 >= tir[0] and ennemi[1]+8 >= tir[1]:
+                ennemis_liste.remove(ennemi)
+                tirs_liste.remove(tir)
+                # on ajoute l'explosion
+                explosions_creation(ennemi[0], ennemi[1])
+
+
+def explosions_creation(x, y):
+    """explosions aux points de collision entre deux objets"""
+    explosions_liste.append([x, y, 0])
+
+
+def explosions_animation():
+    """animation des explosions"""
+    for explosion in explosions_liste:
+        explosion[2] +=1
+        if explosion[2] == 12:
+            explosions_liste.remove(explosion)                
+
 # =========================================================
 # == UPDATE
 # =========================================================
 def update():
     """mise à jour des variables (30 fois par seconde)"""
 
-    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste
+    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste, vies, explosions_liste
 
     # mise à jour de la position du vaisseau
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
@@ -94,8 +139,16 @@ def update():
     ennemis_liste = ennemis_creation(ennemis_liste)
 
     # mise a jour des positions des ennemis
-    ennemis_liste = ennemis_deplacement(ennemis_liste)    
+    ennemis_liste = ennemis_deplacement(ennemis_liste)
 
+    # suppression des ennemis et tirs si contact
+    ennemis_suppression()
+
+    # suppression du vaisseau et ennemi si contact
+    vies = vaisseau_suppression(vies)
+
+    # evolution de l'animation des explosions
+    explosions_animation()    
 
 # =========================================================
 # == DRAW
@@ -106,15 +159,30 @@ def draw():
     # vide la fenetre
     pyxel.cls(0)
 
-    # vaisseau (carre 8x8)
-    pyxel.rect(vaisseau_x, vaisseau_y, 8, 8, 1)
+    # si le vaisseau possede des vies le jeu continue
+    if vies > 0:
 
-    # tirs
-    for tir in tirs_liste:
-        pyxel.rect(tir[0], tir[1], 1, 4, 10)
+        # affichage des vies            
+        pyxel.text(5,5, 'VIES:'+ str(vies), 7)
 
-    # ennemis
-    for ennemi in ennemis_liste:
-        pyxel.rect(ennemi[0], ennemi[1], 8, 8, 8)        
+        # vaisseau (carre 8x8)
+        pyxel.blt(vaisseau_x, vaisseau_y, 0, 0, 0, 8, 8)
+
+        # tirs
+        for tir in tirs_liste:
+            pyxel.blt(tir[0], tir[1], 0, 8, 0, 8, 8)
+
+        # ennemis
+        for ennemi in ennemis_liste:
+            pyxel.blt(ennemi[0], ennemi[1], 0, 0, 8, 8, 8)
+
+        # explosions (cercles de plus en plus grands)
+        for explosion in explosions_liste:
+            pyxel.circb(explosion[0]+4, explosion[1]+4, 2*(explosion[2]//4), 8+explosion[2]%3)            
+
+    # sinon: GAME OVER
+    else:
+
+        pyxel.text(50,64, 'GAME OVER', 7)
 
 pyxel.run(update, draw)
