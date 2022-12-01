90716b9e0fa0ef00a03b1aee1eef86b2672bb8b0
yamayaya <117632350+yamayaya@users.noreply.github.com>
2022-12-01 10:41:26 +0100

    Update cassebriqu.py

--- cassebriqu.py
+++ cassebriqu.py
@@ -2,6 +2,7 @@ import pyxel
 
 pyxel.init(128, 128, title="Nuit du c0de")
 vaisseau_x = 60
+vaisseau_y = 60
 def vaisseau_deplacement(x):
 
     if pyxel.btn(pyxel.KEY_RIGHT):
@@ -10,13 +11,19 @@ def vaisseau_deplacement(x):
     if pyxel.btn(pyxel.KEY_LEFT):
         if (x > 0) :
             x = x - 1
-    return x
+    if pyxel.btn(pyxel.KEY_DOWN):
+        if (y < 120) :
+            y = y + 1
+    if pyxel.btn(pyxel.KEY_UP):
+        if (y > 0) :
+            y = y - 1
+    return x,y
 def update():
-    global vaisseau_x
-    vaisseau_x= vaisseau_deplacement(vaisseau_x)
+    global vaisseau_x, vaisseau_y
+    vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
    
 def draw():
     pyxtreel.cls(0)
-    pyxel.rect(vaisseau_x, 8, 8, 1)
+    pyxel.rect(vaisseau_x, vaisseau_y, 8, 8, 1)
 
 pyxel.run(update, draw)
